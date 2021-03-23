75e3d43cedb90dc4956166e2b37cb08489f7fb45
ijcrawford <ijcrawford@quinnipiac.edu>
2021-03-22 22:25:12 -0400

    Actually Functional App

--- app/src/main/java/edu/quinnipiac/lyricapp/SongHandler.java
+++ app/src/main/java/edu/quinnipiac/lyricapp/SongHandler.java
@@ -8,10 +8,10 @@ import android.graphics.drawable.*;
 public class SongHandler {
 
     private static final int START_SONG = 76877;
-    private static final int END_SONG = 76882;
+    private static final int END_SONG = 76887;
     public static final String Lyrics = "LYRICS";
-    final public static String [] songs = {"", "Chandelier", "Into the Black", "Move Back", "Common People", "Progress"};
-    final public static String [] song_ids = {"", "76878","76879", "76880", "76881", "76882"};
+    final public static String [] songs = {"<select>", "Chandelier", "Into the Black", "Move Back", "Common People", "Progress", "The Ruler", "Creepers", "As You Like It", "Ghetto Love", "Daily Routine"};
+    final public static String [] song_ids = {"", "76878","76879", "76880", "76881", "76882","76916","76917","76885","76908","76909"};
 
     public SongHandler(){
 
@@ -26,7 +26,7 @@ public class SongHandler {
         JSONObject resultJSONObj = new JSONObject(lyricsJsonStr);
         JSONObject responseJSONObj = resultJSONObj.getJSONObject("response");
         JSONObject songJSONObj = responseJSONObj.getJSONObject("song");
-        return songJSONObj.getString("full_title");
+        return songJSONObj.getString("release_date_for_display");
     }
 
 }
